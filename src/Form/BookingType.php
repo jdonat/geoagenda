b9c82cc038c778a605a6e0acc25645bfa654600a
jdonat <j.donatmagnin@it-students.fr>
2024-04-26 15:02:34 +0200

    booking with crud

---
+++ src/Form/BookingType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('beginAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('endAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('title')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
