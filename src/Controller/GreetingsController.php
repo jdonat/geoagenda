5ccc0f5ee73a071a3e3547792af87c8eb00d1acc
jux42 <roujux@gmail.com>
2024-04-26 16:05:31 +0200

    modif twig

---
+++ src/Controller/GreetingsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GreetingsController extends AbstractController
+{
+    #[Route('/greetings', name: 'app_greetings')]
+    public function index(): Response
+    {
+        return $this->render('greetings/index.html.twig', [
+            'controller_name' => 'GreetingsController',
+        ]);
+    }
+}
